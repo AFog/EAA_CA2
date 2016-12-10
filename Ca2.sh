2ae8fb25bf595180bb1eb080d2a737f1a7ebf7c6
John Burns <jzburns@gmail.com>
2016-12-10 14:26:37 +0000

    Minor updates, shell re-run

--- Ca2.sh
+++ Ca2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Idle n Co" >> results.dat
+echo -e "Idle \t n \t Co" >> results.dat
 
 for i in {1..50}
 do
     ./loadtest $i &
     idle=$(mpstat 1 10 | awk 'NR==14{print $12}')
     pkill loadtest
-    echo $idle " " $i " " $(wc -l synthetic.dat | awk '{print $1}') >> results.dat
+    echo -e $idle "\t" $i "\t" $(wc -l synthetic.dat | awk '{print $1}') >> results.dat
 
 done
